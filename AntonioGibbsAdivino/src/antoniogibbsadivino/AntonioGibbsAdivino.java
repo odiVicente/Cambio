888095e2d87cb6be2239f2d5e532f4a8e08f1d9f
AGK21 <73641961+AGK21@users.noreply.github.com>
2020-11-12 20:37:20 +0100

    Add files via upload

---
+++ AntonioGibbsAdivino/src/antoniogibbsadivino/AntonioGibbsAdivino.java
@@ -0,0 +1,29 @@
+
+package antoniogibbsadivino;
+
+import java.util.Scanner;
+
+public class AntonioGibbsAdivino {
+
+  
+    public static void main(String[] args) {
+        
+        int n = 10,cont = 0;
+        int x = (int) (50*Math.random());
+        try ( //System.out.print(x);
+          Scanner sc = new Scanner(System.in)) {
+            do {
+                System.out.print("\nIntroduce el valor de X: ");
+                n = sc.nextInt();
+                if (n > x) {
+                    System.out.print("Te pasaste Amigo");
+                } else if (n < x){
+                    System.out.print("Casi cerca ...");
+                }
+                cont++;
+            } while (n != x);
+            System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
+        }
+    }
+  
+}
