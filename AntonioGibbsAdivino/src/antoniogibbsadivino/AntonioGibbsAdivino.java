a575cc61a9e3a752b51c9013c4ec7be2efafd8d1
Vicente <vicantser@yahoo.es>
2020-11-27 12:56:01 +0100

    Corrección Vicente Antón

--- AntonioGibbsAdivino/src/antoniogibbsadivino/AntonioGibbsAdivino.java
+++ AntonioGibbsAdivino/src/antoniogibbsadivino/AntonioGibbsAdivino.java
@@ -1,29 +1,32 @@
 
 package antoniogibbsadivino;
-
+//Corrección Vicente Antón
 import java.util.Scanner;
-
+import java.util.Random;
 public class AntonioGibbsAdivino {
 
   
     public static void main(String[] args) {
         
-        int n = 10,cont = 10;
-        int x = (int) (50*Math.random());
-        try ( //System.out.print(x);
-          Scanner sc = new Scanner(System.in)) {
+        int n,cont = 0;
+        int max=50;
+        int min=10;
+        Random rand=new Random();
+        int randomNum= rand.nextInt((max-min)+1)+min;
+        //System.out.print(x);
+        Scanner sc = new Scanner(System.in);
             do {
                 System.out.print("\nIntroduce el valor de X: ");
                 n = sc.nextInt();
-                if (n > x) {
+                if (n > randomNum) {
                     System.out.print("Te pasaste Amigo");
-                } else if (n < x){
+                } else if (n < randomNum){
                     System.out.print("Casi cerca ...");
                 }
                 cont++;
-            } while (n != x);
+            } while (n != randomNum);
             System.out.print("Exelente!!! Haz acertado , No. de intentos: "+cont );
-        }
+        
     }
   
 }
